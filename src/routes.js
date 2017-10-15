19fc224c741422ae0e0731fd0e6382c359714048
broerjuang <juangwiantoro@gmail.com>
2017-10-16 00:21:16 +0700

    add centralize routes

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+// @flow
+import React from 'react';
+import {View, Text} from './generals/core-ui';
+import {LandingPage} from './pages';
+
+type Route = {
+  title: string,
+  path: string,
+  Component: React$Element<*>,
+  otherProps?: any,
+};
+type Routes = Array<Route>;
+
+let routes: Routes = [
+  {title: '', path: '/', Component: LandingPage},
+  {title: 'help', path: '/help', Component: DummyPage},
+  {title: 'sign in', path: '/sign-in', Component: DummyPage},
+  {title: 'sign up', path: '/sign-up', Component: DummyPage},
+];
+
+function DummyPage(props) {
+  return (
+    <View style={{marginTop: 100}}>
+      <Text style={{fontSize: 32}}>{props.match.path}</Text>
+    </View>
+  );
+}
+export default routes;
