f37c29055963ffddebf48e2675cefced9b9d93d5
broerjuang <juangwiantoro@gmail.com>
2017-10-16 00:23:50 +0700

    add link into header

--- src/layouts/Header/DesktopHeader.js
+++ src/layouts/Header/DesktopHeader.js
@@ -1,32 +1,31 @@
 // @flow
 
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {StyleSheet} from 'react-native-web';
 
 import {View, Text} from '../../generals/core-ui';
+import routes from '../../routes.js';
 
 import Logo from './CompanyLogo';
-
-let NAVIGATION_TEXT = [
-  'Become our photographer',
-  'Traveller',
-  'How it works',
-  'Help',
-  'sign up',
-  'Sign in',
-];
 
 type Props = {}; // TODO: put Navigation Link here
 
 function DesktopHeader(props: Props) {
   return (
     <View style={styles.container}>
-      <Logo key="logo" />
+      <Link to="/" style={StyleSheet.flatten(styles.noTextDecoration)}>
+        <Logo key="logo" />
+      </Link>
       <View style={styles.flexRow}>
-        {NAVIGATION_TEXT.map(nav => (
-          <Text style={{marginRight: 30}} key={nav}>
-            {nav.toUpperCase()}
-          </Text>
+        {routes.map(route => (
+          <Link
+            key={route.title}
+            to={route.path}
+            style={StyleSheet.flatten(styles.noTextDecoration)}
+          >
+            <Text style={styles.navText}>{route.title.toUpperCase()}</Text>
+          </Link>
         ))}
       </View>
     </View>
@@ -42,6 +41,13 @@ let styles = StyleSheet.create({
   },
   flexRow: {
     flexDirection: 'row',
+  },
+  noTextDecoration: {
+    textDecoration: 'none',
+  },
+  navText: {
+    marginRight: 30,
+    color: 'black',
   },
 });
 
